fce8cfad2623afebefbe16313f3907df66081139
George Hall <george_edward_hall@hotmail.co.uk>
2024-02-08 08:03:22 +1100

    Added docker watch command

--- docker/install.sh
+++ docker/install.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env sh
 
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg
+# sudo apt update
+# sudo apt install apt-transport-https ca-certificates curl gnupg
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg --yes
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# sudo apt update
+# sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo usermod -aG docker ${USER}
+# sudo usermod -aG docker ${USER}
 
-echo "" >> $1
+# echo "" >> $1
 
 up="alias up='docker compose up -d'"
 down="alias down='docker compose down'"
+watchdocker="alias wd=\"watch -n3 \\\"docker ps --format 'table {{.ID}}\\\t{{.Status}}\\\t{{.Names}}\\\t{{.Ports}}'\\\"\""
 
 grep -qxF "$up" $1 || echo $up >> $1
 grep -qxF "$down" $1 || echo $down >> $1
+grep -qxF "$watchdocker" $1 || echo $watchdocker >> $1
