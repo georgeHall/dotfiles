bf14f348c93aff953b4013e934781fc067d3b18b
George Hall <george_edward_hall@hotmail.co.uk>
2024-03-09 17:21:27 +1100

    Temp

--- oh-my-zsh/.zshrc
+++ oh-my-zsh/.zshrc
@@ -6,7 +6,6 @@ plugins=(
     git
     zsh-autosuggestions
     zsh-syntax-highlighting
-    tmux    
 )
 
 alias ll='ls -al'
@@ -14,6 +13,6 @@ alias ll='ls -al'
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/usr/local/go/bin
 
-export EDITOR=vim
+export EDITOR=nvim
 export VISUAL="$EDITOR"
 
--- python/install.sh
+++ python/install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
-
-rm $(pwd)/get-pip.py
-
-python3 -m pip install --user ansible
-
+if ! pip --version
+then
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3 get-pip.py --user
+    rm $(pwd)/get-pip.py
+fi
