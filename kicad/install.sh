23ad7ca5c2c0b722fa35117682cd72cd30c4d055
George Hall <george_edward_hall@hotmail.co.uk>
2024-08-21 19:45:17 +1000

    Make files runnable

--- kicad/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! kicad
-do
-    sudo add-apt-repository -y ppa:kicad/kicad-8.0-releases
-    sudo apt update
-    sudo apt install -y kicad
-done
--- setup.sh
+++ setup.sh
@@ -16,13 +16,12 @@ sh $(pwd)/git/install.sh
 sh $(pwd)/code/install.sh
 sh $(pwd)/python/install.sh
 sh $(pwd)/k8s/install.sh
-sh $(pwd)/minikube/install.sh
-sh $(pwd)/awscli/install.sh
+#sh $(pwd)/minikube/install.sh
+#sh $(pwd)/awscli/install.sh
 #sh $(pwd)/tmux/install.sh
-sh $(pwd)/kicad/install.sh
 sh $(pwd)/docker/install.sh "$shell_file"
-sh $(pwd)/gcloud/install.sh "$shell_file"
-sh $(pwd)/hashicorp/install.sh "$shell_file"
+#sh $(pwd)/gcloud/install.sh "$shell_file"
+#sh $(pwd)/hashicorp/install.sh "$shell_file"
 sh $(pwd)/golang/install.sh "$shell_file"
 
 sudo apt autoremove -y
